321a7054ccf8f9a9312ae2fac8b19d0fb5eb24cf
selvaram ganesh <gsriram7@gmail.com>
2017-11-15 06:19:45 -0800

    Parsing input file and testing it

--- src/test/scala/ParserTest.java
+++ src/test/scala/ParserTest.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Arrays;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ParserTest {
 
@@ -52,12 +56,25 @@ public class ParserTest {
         Literal[] literals = sentence.getLiterals();
         assertThat(literals.length, is(5));
         assertThat(Arrays.stream(literals).filter(l -> l.isNegated).count(), is(4L));
-        assertThat(checkLiteralEquality(literals[0], new Literal("American", new Term[] {new Variable("x")}, true)), is(true));
-        assertThat(checkLiteralEquality(literals[2], new Literal("Sells", new Term[] {new Variable("x"), new Variable("y"), new Variable("z")}, true)), is(true));
-        assertThat(checkLiteralEquality(literals[3], new Literal("Enemy", new Term[] {new Variable("z"), new Constant("America")}, true)), is(true));
+        assertThat(checkLiteralEquality(literals[0], new Literal("American", new Term[]{new Variable("x")}, true)), is(true));
+        assertThat(checkLiteralEquality(literals[2], new Literal("Sells", new Term[]{new Variable("x"), new Variable("y"), new Variable("z")}, true)), is(true));
+        assertThat(checkLiteralEquality(literals[3], new Literal("Enemy", new Term[]{new Variable("z"), new Constant("America")}, true)), is(true));
+    }
+
+    @Test
+    public void shouldTestInputFileParsing() throws Exception {
+        for (int i = 1; i <= 3; i++) {
+            File file = new File(this.getClass().getResource(String.format("ip%d.txt", i)).getFile());
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            assertTrue(br.lines().map(s -> Parser.parseSentence(s).toString().equals(s)).reduce(Boolean::logicalAnd).get());
+
+            br.close();
+        }
     }
 
     boolean checkLiteralEquality(Literal a, Literal b) {
-        return a.name.equals(b.name) && Arrays.equals(a.getTerms(), b.getTerms()) && a.isNegated==b.isNegated;
+        return a.name.equals(b.name) && Arrays.equals(a.getTerms(), b.getTerms()) && a.isNegated == b.isNegated;
     }
 }
